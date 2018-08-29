79d1e45934a71a42abe441399ecbbccb8cb34436
Tianhao <zhangth1990@hotmail.com>
2018-08-29 12:55:43 -0600

    created searchActivity

--- app/src/main/java/com/example/tianhao/instagramclone/SearchActivity.java
+++ app/src/main/java/com/example/tianhao/instagramclone/SearchActivity.java
@@ -1,28 +1,24 @@
 package com.example.tianhao.instagramclone;
 
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.example.tianhao.instagramclone.Utils.BottomNavigationViewHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
-public class HomeActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity{
+    private static final String TAG = "SearchActivity";
 
-    private  static  final String TAG="HomeActivity";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_home);
-        Log.d(TAG,"onCreate:starting.");
-
+        Log.d(TAG,"onCreate: started.");
         setupBottomNavigationView();
     }
-
-    /**
-     * BottomNavigationView setup
-     */
     private  void setupBottomNavigationView(){
         Log.d(TAG,"setupBottomNavigationView:setting up BottomNavigationView");
         BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
