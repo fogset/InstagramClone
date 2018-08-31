11795733859e464e6d0ff82e856e988bdbad525c
Tianhao <zhangth1990@hotmail.com>
2018-08-30 22:54:26 -0600

    created shareActivity

---
+++ app/src/main/java/com/example/tianhao/instagramclone/ShareActivity.java
@@ -0,0 +1,27 @@
+package com.example.tianhao.instagramclone;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.example.tianhao.instagramclone.Utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class ShareActivity extends AppCompatActivity{
+    private static final String TAG = "ShareActivity";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_home);
+        Log.d(TAG,"onCreate: started.");
+        setupBottomNavigationView();
+    }
+    private  void setupBottomNavigationView(){
+        Log.d(TAG,"setupBottomNavigationView:setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+    }
+}
