e5eadc10af93d515dead1ac125bf7718fed6a3e7
Tianhao <zhangth1990@hotmail.com>
2018-09-05 19:33:23 -0600

    added setupViewPager in homeactivity to add 3 tabs camera,home messages

--- app/src/main/java/com/example/tianhao/instagramclone/Home/HomeActivity.java
+++ app/src/main/java/com/example/tianhao/instagramclone/Home/HomeActivity.java
@@ -1,11 +1,13 @@
 package com.example.tianhao.instagramclone.Home;
 
 import android.content.Context;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.tianhao.instagramclone.R;
 import com.example.tianhao.instagramclone.Utils.BottomNavigationViewHelper;
@@ -23,6 +25,16 @@ public class HomeActivity extends AppCompatActivity {
         Log.d(TAG,"onCreate:starting.");
 
         setupBottomNavigationView();
+        setupViewPager();
+    }
+    //Responsible for adding the 3 tabs: Camera, Home, Messages
+    private void setupViewPager(){
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CameraFragment());
+        adapter.addFragment(new HomeFragment());
+        adapter.addFragment(new MessagesFragment());
+        ViewPager viewPager =(ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
     }
 
     /**
